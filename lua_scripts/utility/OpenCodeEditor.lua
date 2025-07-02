a55c89c03c5e2f72f7a7578bec12b1890478aaa1
Ernest Placido <ernestplacido05@gmail.com>
2025-07-02 22:30:10 +0100

    Cleaner OpenCodeEditor

--- lua_scripts/utility/OpenCodeEditor.lua
+++ lua_scripts/utility/OpenCodeEditor.lua
@@ -1,51 +1,47 @@
 function OpenCodeEditor(settings, projectPath, filePath)
-    local indexPath = System:join(projectPath, "lua_scripts", "index.lua")
+    local editorConfigs = {
+        ["codeEditor_VSCode"]         = { cmd = "code",          project = true, file_flag = "-g" },
+        ["codeEditor_VSCodeInsiders"] = { cmd = "code-insiders", project = true, file_flag = "-g" },
+        ["codeEditor_CodeOSS"]        = { cmd = "code-oss",      project = true, file_flag = "-g" },
+        ["codeEditor_Atom"]           = { cmd = "atom",          project = true, file_flag = nil  },
+        ["codeEditor_Sublime"]        = { cmd = "subl",          project = true, file_flag = nil  },
+        ["codeEditor_Sublime-Text"]   = { cmd = "sublime_text",  project = true, file_flag = nil  },
+        ["codeEditor_Notepad"]        = { cmd = "notepad",       project = false, file_flag = nil }
+    }
+
+    local editorKey = settings.codeEditor
+    if not editorKey or not editorConfigs[editorKey] then
+        return -- No valid editor
+    end
+    local config = editorConfigs[editorKey]
 
     local projectData = System:readJSON(System:join(projectPath, "project.json"))
-    
-    local indexAppend = ""
-    if filePath then
-        indexAppend = string.concat(" \"", filePath, "\"")
-    elseif projectData.uninitiated then
-        indexAppend = string.concat(" \"", indexPath, "\"")
+    local isUninitiated = projectData and projectData.uninitiated
+
+    local fileToOpen = filePath
+    if not fileToOpen and isUninitiated then
+        fileToOpen = System:join(projectPath, "lua_scripts", "index.lua")
     end
 
-    if settings.codeEditor then
-        if settings.codeEditor == "codeEditor_VSCode" then
-            if projectData.uninitiated and not filePath then
-                indexAppend = string.concat(" -g \"", indexPath, "\"")
-            end
-            System:execute(string.concat("code \"", projectPath, "\"", indexAppend))
-        elseif settings.codeEditor == "codeEditor_VSCodeInsiders" then
-            if projectData.uninitiated and not filePath then
-                indexAppend = string.concat(" -g \"", indexPath, "\"")
-            end
-        elseif settings.codeEditor == "codeEditor_CodeOSS" then
-            if projectData.uninitiated and not filePath then
-                indexAppend = string.concat(" -g \"", indexPath, "\"")
-            end
-            System:execute(string.concat("code-oss \"", projectPath, "\"", indexAppend))
-        elseif settings.codeEditor == "codeEditor_Atom" then
-            if projectData.uninitiated and not filePath then
-                indexAppend = string.concat(" -g \"", indexPath, "\"")
-            end
-            System:execute(string.concat("atom \"", projectPath, "\"", indexAppend))
-        elseif settings.codeEditor == "codeEditor_Sublime" then
-            if projectData.uninitiated and not filePath then
-                indexAppend = string.concat(" -g \"", indexPath, "\"")
-            end
-            System:execute(string.concat("subl \"", projectPath, "\"", indexAppend))
-        elseif settings.codeEditor == "codeEditor_Sublime-Text" then
-            if projectData.uninitiated and not filePath then
-                indexAppend = string.concat(" -g \"", indexPath, "\"")
-            end
-            System:execute(string.concat("sublime_text \"", projectPath, "\"", indexAppend))
-        elseif settings.codeEditor == "codeEditor_Notepad" then
-            System:execute(string.concat("notepad \"", indexPath, "\""))
+    local args = { config.cmd }
+
+    if config.project then
+        table.insert(args, '"' .. projectPath .. '"')
+    end
+
+    if fileToOpen then
+        if config.file_flag then
+            table.insert(args, config.file_flag)
+            table.insert(args, '"' .. fileToOpen .. '"')
+        else
+            table.insert(args, '"' .. fileToOpen .. '"')
         end
     end
 
-    if projectData.uninitiated and not filePath then
+    local command = table.concat(args, " ")
+    System:execute(command)
+
+    if isUninitiated then
         projectData.uninitiated = nil
         System:writeFile(System:join(projectPath, "project.json"), projectData)
     end
