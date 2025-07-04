3ec1987c4b65811c56339172b40294a9d17dd30d
Ernest Placido <ernestplacido05@gmail.com>
2025-07-04 01:38:32 +0100

    Instantaneous window raising

--- lua_scripts/utility/BringGameToFront.lua
+++ lua_scripts/utility/BringGameToFront.lua
@@ -2,7 +2,5 @@ for i = 1, #Creator.worlds do
     local world = Creator.worlds[i]
     local prev = world.alwaysOnTop
     world.alwaysOnTop = true
-    world:wait(0.1):next(function()
-        world.alwaysOnTop = prev
-    end)
+    world.alwaysOnTop = prev
 end
