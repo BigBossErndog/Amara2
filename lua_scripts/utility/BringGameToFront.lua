2c4366ea710f53bdcabde007db1bccd867dc056a
Ernest Placido <ernestplacido05@gmail.com>
2025-07-04 01:36:47 +0100

    Restoring previous alwaysOnTop state

--- lua_scripts/utility/BringGameToFront.lua
+++ lua_scripts/utility/BringGameToFront.lua
@@ -1,7 +1,8 @@
 for i = 1, #Creator.worlds do
     local world = Creator.worlds[i]
+    local prev = world.alwaysOnTop
     world.alwaysOnTop = true
     world:wait(0.1):next(function()
-        world.alwaysOnTop = false
+        world.alwaysOnTop = prev
     end)
 end
