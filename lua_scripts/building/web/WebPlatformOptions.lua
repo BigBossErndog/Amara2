f22d44442652aeefa7a9c6d2edf5c714e74a6137
Ernest Placido <ernestplacido05@gmail.com>
2025-08-02 19:20:21 +0100

    Working on web build

--- lua_scripts/building/web/WebPlatformOptions.lua
+++ lua_scripts/building/web/WebPlatformOptions.lua
@@ -1,4 +1,10 @@
 Nodes:define("WebPlatformOptions", "UIWindow", {
     width = 256,
-    height = 106
+    height = 106,
+
+    onConfigure = function(self, config)
+        if config.projectPath then
+            self.props.projectPath = config.projectPath
+        end
+    end
 })
