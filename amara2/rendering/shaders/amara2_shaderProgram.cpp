ee236ea8da7bf1632d1e698a9be59f1683d82eff
Ernest Placido <ernestplacido05@gmail.com>
2026-01-15 16:22:34 +0000

    Normalized shader program color uniform without alpha

--- amara2/rendering/shaders/amara2_shaderProgram.cpp
+++ amara2/rendering/shaders/amara2_shaderProgram.cpp
@@ -190,7 +190,7 @@ namespace Amara {
                     return;
                 }
                 if (json_has(value, "r", "g", "b")) {
-                    uniforms[name] = Vector4(value["r"], value["g"], value["b"], 1.0f);
+                    uniforms[name] = Vector4(value["r"].get<float>() / 255.0f, value["g"].get<float>() / 255.0f, value["b"].get<float>() / 255.0f, 1.0f);
                     return;
                 }
                 if (json_has(value, "x", "y", "z", "w")) {
